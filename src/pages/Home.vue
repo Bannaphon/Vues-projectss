e2df428a312dfbb897e625deed937307373e29ff
Bannaphon <112239420+Bannaphon@users.noreply.github.com>
2022-10-08 21:13:08 +0700

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <navbar />
+  <section class="home">
+
+    <div class="image">
+        <img src="images/day6-.png" alt="">
+    </div>
+
+    <div class="content">
+        <h3>hi, Bannaphon Kongthip</h3>
+        <span>Fortune favors the prepared mind.</span>
+        <p>Blue!</p>
+        <a href="about.html" class="btn"> about me <i class="fas fa-user"></i> </a>
+    </div>
+
+</section>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+export default {
+  components: { Navbar },
+
+}
+</script>
+
+<style>
+
+</style>
